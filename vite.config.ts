d718405ae3899f04dbb98c949abcf53d2ad8e879
yuandashuai <yuandashuai@hualala.com>
2024-06-19 09:20:19 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import {resolve} from "path"
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/common-admin/',
@@ -22,6 +26,21 @@ export default defineConfig({
         replacement: resolve(__dirname, 'src')
       }
     ]
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      }
+    }
   },
   server: {
     host: '0.0.0.0',
